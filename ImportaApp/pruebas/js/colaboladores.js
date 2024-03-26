3b61f6b51dda5b29ae465dc18e99d6b45316e33d
Esteban Leon Aguilar <esteban.aguilar2005@hotmail.com>
2024-03-26 03:40:07 -0500

    intento de mejoras

---
+++ ImportaApp/pruebas/js/colaboladores.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $('.logos_colaboradores').slick({
+        dots: false,
+        infinite: true,
+        speed: 200,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
