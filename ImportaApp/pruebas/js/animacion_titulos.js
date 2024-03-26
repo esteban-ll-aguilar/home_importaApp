3f70584cfc18ce8fe6b01c1c7b027dc3a8766247
Esteban Leon Aguilar <esteban.aguilar2005@hotmail.com>
2024-03-26 03:14:30 -0500

    responsive en algunas paprtes

--- ImportaApp/pruebas/js/animacion_titulos.js
+++ ImportaApp/pruebas/js/animacion_titulos.js
@@ -1,35 +1,21 @@
-window.onload = function () {
-    const texts = document.querySelectorAll('.text-container h1');
+function animateTexts() {
+    const texts = ["Ecosistema de negocios internacionales", "Servicio bajo demanda", "Logística verde", "Blockchain en la cadena de suministros", "Economía colaborativa"];
+    let index = 0;
+    const textContainer = document.querySelector('.text-container');
 
-    // Duplica los cajones para crear un scroll infinito
-    const cajonesScroll = document.querySelector('.titulos');
-    cajonesScroll.innerHTML += cajonesScroll.innerHTML;
-
-
-
-    // Función para manejar el reinicio de la animación
-    function restartAnimation() {
-        // Reinicia la animación de scroll horizontal
-        cajonesScroll.style.animation = 'none';
-        cajonesScroll.offsetHeight; /* Activa un repaint, sin esto la animación no se reinicia */
-        cajonesScroll.style.animation = 'scroll 0s linear infinite';
+    function showNextText() {
+        textContainer.textContent = texts[index];
+        gsap.fromTo(textContainer, { opacity: 0 }, { opacity: 1, duration: 1 });
+        gsap.to(textContainer, { opacity: 0, duration: 0.5, delay: 1 });
+        index = (index + 1) % texts.length; // Avanzar al siguiente texto circularmente
     }
 
-    // Escucha el evento 'animationiteration' para reiniciar la animación
-    cajonesScroll.addEventListener('animationiteration', restartAnimation);
+    // Mostrar el primer texto al iniciar
+    showNextText();
 
-    // Función para mostrar y ocultar los textos
-    async function animateTexts() {
-        while (true) {
-            for (let i = 0; i < texts.length; i++) {
-                texts[i].style.opacity = '1'; // Mostrar el h1
-                await new Promise(resolve => setTimeout(resolve, 3000)); // Esperar 2 segundos
-                texts[i].style.opacity = '0'; // Ocultar el h1
-                await new Promise(resolve => setTimeout(resolve, 500)); // Esperar medio segundo
-            }
-        }
-    }
+    // Repetir la animación cada 2 segundos
+    setInterval(showNextText, 2000);
+}
 
-    // Inicia la animación de los textos
-    animateTexts();
-};
+// Inicia la animación de los textos
+animateTexts();
---
+++ ImportaApp/pruebas/js/cajones_carrucel_servicio.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
