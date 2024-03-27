1843fb6d4cf073311b139d4c8096dd49947bdbe5
lualbertovargas <81051067+lualbertovargas@users.noreply.github.com>
2024-03-26 21:10:16 -0500

    header

---
+++ ImportaApp/pruebas/js/header_behavior.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.header').addClass('header-scrolled');
+            // Cambiar el logo al hacer scroll
+            $('.header-logo').attr('src', 'images/Logo Blanco letra negra.png');
+
+        } else {
+            $('.header').removeClass('header-scrolled');
+            // Volver al logo original cuando se está en la parte superior de la página
+            $('.header-logo').attr('src', 'images/Logo Transparente letra blanca.png');
+        }
+    });
+});
+
